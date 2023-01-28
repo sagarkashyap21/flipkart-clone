d2fa6be8eb15831f7cf1667a5d934ce5a86e2628
sagarkashyap21 <111484724+sagarkashyap21@users.noreply.github.com>
2023-01-29 00:44:11 +0530

    Update Navbar.js

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
 <div style={{border: "",alignItems:"center",height:"60px",display:"flex",backgroundColor:"#2874f0" }}>
     <ul style={{border: "",marginTop:"10px",marginLeft:"150px",listStyle:"none",display:"flex",alignItems:"center"}}>
         <li><img style={{height:"40px",width:"60px"}} src="https://www.freepnglogos.com/uploads/flipkart-logo-png/flipkart-logo-transparent-png-download-0.png" alt="" /></li>
-        <li style={{border:"",marginLeft:"20px",height:"35px",backgroundColor:"white",display:"flex",alignItems:"center"}}><input style={{width:"400px",border:"0"}} type="search" placeholder="Search for products,brands and more"  /><i style={{color:"blue",marginLeft:"40px",marginRight:"10px"}} class="fa fa-solid fa-magnifying-glass"></i></li>
+        <li style={{border:"",marginLeft:"20px",height:"35px",backgroundColor:"white",display:"flex",alignItems:"center"}}><input style={{width:"400px",outline:"none",border:"0"}} type="search" placeholder="Search for products,brands and more"  /><i style={{color:"blue",marginLeft:"40px",marginRight:"10px"}} class="fa fa-solid fa-magnifying-glass"></i></li>
         <li style={{backgroundColor:"white",marginLeft:"20px",height:"35px",width:"100px",display:"flex",justifyContent:"center"}}><button style={{color:"blue",backgroundColor:"white",fontSize:"20px",border:"0"}}>Login</button></li>
     </ul>
     <ul style={{border:"",marginTop:"5px",width:"370px",display:"flex",listStyle:"none",justifyContent:"space-between"}}>
